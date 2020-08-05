b8de22807ba90e91aab21cb5c841658276940696
KelvinLopes <kelvin7android@gmail.com>
2020-08-05 00:33:00 +0000

    Initial Page TeacherList and TeacherForm

--- nlw/web/src/pages/Landing/index.tsx
+++ nlw/web/src/pages/Landing/index.tsx
@@ -4,6 +4,7 @@ import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHearticon from '../../assets/images/icons/purple-heart.svg';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -25,15 +26,15 @@ function Landing() {
                         alt="Plataforma de estudos" 
                         />
                         <div className="buttons-container">
-                            <a href="#" className="study" >
+                            <Link to="/study" className="study" >
                                 <img src={studyIcon} alt="Estudar" />
                                 Estudar
-                            </a>
+                            </Link>
 
-                             <a href="#" className="give-classes" >
+                             <Link to="/give-classes" className="give-classes" >
                                 <img src={giveClassesIcon} alt="Dar aulas" />
                                 Dar aulas
-                            </a>
+                        </Link>
                     </div>
                   <span className="total-connections">
                       Total de 200 conexôes já realizadas
