d4977ceb2c15c9f3570dfa02d255d341baaf428d
KelvinLopes <kelvin7android@gmail.com>
2020-08-06 01:47:18 +0000

    Update server

--- nlw/server/src/server.ts
+++ nlw/server/src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello Word')
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (request, response) => {
+
+    return response.json({ message: 'Hello Word' });
+});
+
+app.listen(3333);
