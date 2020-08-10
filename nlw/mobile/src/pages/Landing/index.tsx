08be2e47d58a82a13acb94c23de76496c8d4eddc
KelvinLopes <kelvin7android@gmail.com>
2020-08-10 00:51:27 +0000

    Add Page GiveClasses

--- nlw/mobile/src/pages/Landing/index.tsx
+++ nlw/mobile/src/pages/Landing/index.tsx
@@ -1,6 +1,7 @@
 import React from  'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 import styles from  './styles';
 
 import landingImg from '../../assets/images/landing.png';
@@ -30,18 +31,18 @@ function Landing() {
             </Text>
 
         <View style={styles.buttonsContainer}>
-            <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+            <RectButton style={[styles.button, styles.buttonPrimary]}>
                 <Image source={studyIcon} />
                 <Text>Estudar</Text>
-            </TouchableOpacity>
+            </RectButton>
 
-             <TouchableOpacity 
+             <RectButton 
                 onPress={handleNavigateToGiveClassesPage} 
                 style={[styles.button, styles.buttonSecondary]}
                 >
                 <Image source={giveClassesIcon} />
                 <Text>Dar aulas</Text>
-            </TouchableOpacity>
+            </RectButton>
         </View>
 
         <Text style={styles.totalConnections}>
